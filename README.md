60eabdba6cebb4d41ed5a8b31fc34d3803a3003a
Elton <eltonp3103@gmail.com>
2021-07-04 00:49:52 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# yolo-tree
+
+A [linktree](https://linktr.ee/) clone.
+
+## Usage
+
+This is a template repo. You can use it to host your own link tree for free (using GitHub pages for example).
+
+1. Click the "Use this template button"
+2. Update the `links.json` file with your links.
+3. Customize the look of your link tree
+4. Deploy (using [GitHub pages](https://docs.github.com/en/pages/getting-started-with-github-pages/creating-a-github-pages-site) for example)
---
+++ index.js
@@ -0,0 +1,92 @@
+class YoloTree extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const leaves = this.querySelectorAll("li");
+    const subtrees = this.querySelectorAll("ul");
+
+    for (const subtree of subtrees) {
+      subtree.classList.add("hide");
+    }
+
+    for (const leaf of leaves) {
+      const subtree = leaf.querySelector("ul");
+      if (subtree !== null) {
+        leaf.addEventListener("click", (e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          subtree.classList.toggle("hide");
+        });
+      }
+    }
+  }
+}
+
+customElements.define("yolo-tree", YoloTree);
+
+const app = document.getElementById("app");
+
+async function fetchLinks() {
+  const result = await fetch("/links.json");
+  const data = await result.json();
+  return data;
+}
+
+function makeLink(txt, href) {
+  const anchor = document.createElement("a");
+  anchor.innerHTML = txt;
+  anchor.href = href;
+  anchor.target = "_blank";
+  return anchor;
+}
+
+function makeIcon(txt, src) {
+  const icon = document.createElement("img"); 
+  icon.src = src;
+  icon.alt = txt;
+  icon.classList.add("icon");
+  return icon;
+}
+
+function buildTree(links, yolo = true) {
+  let tag;
+  if (yolo) {
+    tag = "yolo-tree";
+  } else {
+    tag = "ul";
+  }
+  const tree = document.createElement(tag);
+  for (const key in links) {
+    const leaf = document.createElement("li");
+    const value = links[key];
+    if (typeof value === "object") {
+      if (value.link) {
+        leaf.appendChild(makeLink(key, value.link));
+        if (value.icon) {
+          leaf.appendChild(makeIcon(key, value.icon));
+        }
+      } else {
+        leaf.innerHTML = key;
+        leaf.classList.add("plump");
+        const subtree = buildTree(links[key], false);
+        leaf.appendChild(subtree);
+        if (value.icon) {
+          leaf.appendChild(makeIcon(key, value.icon));
+        }
+      }
+    } else {
+      leaf.appendChild(makeLink(key, value));
+    }
+    tree.appendChild(leaf);
+  }
+  return tree;
+}
+
+(async () => {
+  const links = await fetchLinks();
+  const tree = buildTree(links);
+  const app = document.getElementById("app");
+  app.appendChild(tree);
+})();
